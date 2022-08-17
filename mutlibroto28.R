7969cf01871b4594400cb24157573c815d5d74d6
Leticia Hagale <111390496+leticiahagale@users.noreply.github.com>
2022-08-16 23:10:03 -0300

    Update multibroto28

--- mutlibroto28.R
+++ mutlibroto28.R
@@ -1,81 +1,78 @@
-#### Trabalho final ####
+#### Trabalho final Discplina R ####
+###Analise de inducao de multibroto de capim elefante apos 28 dias###
 
 #diretorio
 setwd("C:/Displina_R_2022.1")
 dir()
 
 #importando dados
-dados=read.csv(file= "trabfinal.csv" ,header = TRUE, sep = ";")
+data=read.csv(file="trabfinal.csv",header = TRUE, sep = ";")
 
-#inspecao dados
-str(dados)
-dados$tratamento=as.factor(dados$tratamento)
-summary(dados)
-med=tapply(dados$multibroto,dados$tratamento,mean)
-med
+#inspecao do banco de dados
+str(data)
+data$treatment=as.factor(data$treatment)
+summary(data)
+
+#Inspecao grafica dos dados
+boxplot(multisprout~treatment)
 
 #transformando dados em objetos
-attach(dados)
+attach(data)
 
-#hipoteses
-#h0=nao ha diferenca significativa entre a inducao de multibrotos com a variacao de tratamentos
-#h1=ha diferenca significativa entre a inducao de multibrotos de acordo com o tratamento
-
-#variaveis
+##variaveis estatisticas
 #tratamento=variavel qualitativa,categorica -> preditora,independente
 #multibro=variavel quantitativa -> resposta,dependente
 
 #o teste realizado -> ANOVA
+#necessario realizar o teste antes do teste de normalidade dos residuos
 
-#TESTE DE PRESSUPOSTOS
-#h0=os dados sao normais
+result=aov(multisprout~treatment) 
+ls(result) 
+
+###TESTE DE PRESSUPOSTOS
 
 library(car)
 
-##Homogeneidade 
-leveneTest(multibroto~tratamento)
-#df=4 , F-value=  2.5593 p-value=0.05623
-#aceita h0,normal pois p>0.05
+##Homogeneidade das amostras
+leveneTest(multisprout~treatment)
+#df=4 , F-value=  2.142  p-value=0.0969
+#aceita h0,homogeneo pois p>0.05
 
-
-#anova (necessaria para o proximo pressuposto)
-resultado=aov(multibroto~tratamento) #roda o teste primeiro e olha o resultado após a análise dos resisuos gerados aqui.
-ls(resultado) #de onde pega os residuos
-
+##Normlidade dos residuos
 #Análise gráfica
 
-qqnorm(resultado$residuals,col = "black",pch=16)
-qqline(resultado$residuals,col = "red",lty=2)
+qqnorm(result$residuals,col = "black",pch=16)
+qqline(result$residuals,col = "red",lty=2)
 
-hist(resultado$residuals,col = "blue")
+hist(result$residuals,col = "blue")
 
 #Analise estatistica
 
-#normlidade dos residuos
-shapiro.test(resultado$residuals)#H0=A distribuicao é normal,desejamos o p>0,05
-#O valor de p=0.0591,ou seja, maior que 0,05. Aceita H0.
-##Portanto aceita a normalidade 
+shapiro.test(result$residuals)
+#W=0.97649 e p-value=0.5773
+#aceita H0,normal pois p>0.05
+
 
 ###Analisando o teste ANOVA
-summary(resultado)
+summary(result)
+#F value=11.9 p-value=3.74e-06 ***
 #rejeita h0,existe diferenca significativa entre os tratamentos (geral)
 
-resultado2=lm(multibroto~tratamento) #para o valor de p em relacao ao tratamento controle comparado com cada um
+second_result=lm(multisprout~treatment) #para o valor de p de cada tratamento em relacao ao controle 
 
-summary(resultado2)
-#Adjusted R-squared:  0.3863 ,SIGNIFICA QUE 38% É JUSTIFICADO PELO TRATAMENTO.
+summary(second_result)
+#Adjusted R-squared:  0.5344 ,SIGNIFICA QUE 53% É JUSTIFICADO PELO TRATAMENTO.
 #todos os tratamentos tem diferenca signficativa com o controle(esperado)
 
-boxplot(multibroto~tratamento)
-points(1:3,med, 1, col = "blue", pch = 19, type = "p")
 
-###Teste de post hoc, Teste de Tukey mostra os resultados comparando cada um dos tratamentos entre si
-
-TukeyHSD(resultado)
-##Tukey multiple comparisons of means
-#95% family-wise confidence level
+##Teste a posteriori _> Teste de Tukey 
 #p deve ser menor que 0.05
 
-#TRAT07-TRAT01 p=0.0007954
-#TRAT06-TRAT01 p=0.0191949
-#A diferenca sifnificativa foi encontrada entre o tratamento 7 e o controle e o tratamento 6 e o controle.
+TukeyHSD(result)
+
+#A diferenca sifnificativa foi encontrada entre os tratamentos e o controle,sendo o menos p entre o TRAT2 e o TRAT0. Comparando os demais concluimos que os TRAT3 E 4 nao possuem diferenca significativa em relacao ao 1 e o 2 com excecao ao TRAT4 E O 2(0.0341707) e que entre o TRAT1 E 2 ha uma diferenca significativa.
+
+citation() #referenciar o R em artigo
+
+#####FIM DE ANALISE ###
+
